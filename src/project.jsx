1c99a6a197f6058c930ba26604abfe8ddf460056
asadkhaksar1122 <asadkhaksar1122@gmail.com>
2024-09-21 03:55:39 -0700

    fix

--- src/project.jsx
+++ src/project.jsx
@@ -43,28 +43,28 @@ export default function Project() {
       description:
         "Our Password Manager app, built with the MERN stack (MongoDB, Express.js, React, Node.js), securely stores and manages passwords. Users must enter a secret key to view or edit passwords, ensuring authorized access. The intuitive React interface makes password management easy and secure.",
       githubLink: "https://github.com/asadkhaksar1122/Password-Manager",
-      imageurl: "./images/logo.png",
+      imageurl: "./navbaricon/logo.png",
     },
     {
       name: "Task Reminder",
       description:
         'This is a MERN stack task reminder application that leverages MongoDB with Mongoose for the backend, Express for the server-side framework, React for the frontend, and Node.js for the runtime environment. Tasks can be added, edited, and deleted, with the ability to mark them as done using a "done" key for user reference.',
       githubLink: "https://github.com/asadkhaksar1122/Task-Reminder",
-      imageurl: "./images/taskreminder.webp",
+      imageurl: "./navbaricon/taskreminder.webp",
     },
     {
       name: "I-Notebook",
       description:
         "Welcome to our modern platform built with MERN stack for efficient user-specific note management. Enjoy seamless storage, deletion, and editing upon login, with a focus on simplicity and security. Benefit from a sleek user interface for easy organization of ideas, tasks, and reminders. Trust us for a secure and intuitive note-taking experience that prioritizes productivity and organization",
       githubLink: "https://github.com/asadkhaksar1122/I-notebook",
-      imageurl: "./images/inotebook.avif",
+      imageurl: "./navbaricon/inotebook.avif",
     },
     {
       name: "Coding Quiz",
       description:
         "The coding quiz app in React utilizes useReducer for managing complex state logic efficiently. By dispatching actions based on user interactions, handling language quizzes becomes organized and predictable. This leads to a maintainable codebase and improves user experience while navigating quizzes.",
       githubLink: "https://github.com/asadkhaksar1122/Coding-quiz",
-      imageurl: "./images/quiz.png",
+      imageurl: "./navbaricon/quiz.png",
       livelink: "https://asadkhaksar1122.github.io/Coding-quiz/",
     },
     {
@@ -72,7 +72,7 @@ export default function Project() {
       description:
         "Experience the classic Simon Game brought to life with HTML, CSS, and JavaScript. Test your memory and reflexes as you follow and repeat the pattern of lights and sounds. Have fun challenging yourself with this exciting electronic game!",
       githubLink: "https://github.com/asadkhaksar1122/simon-game",
-      imageurl: "./images/simon.jpg",
+      imageurl: "./navbaricon/simon.jpg",
       livelink: "https://asadkhaksar1122.github.io/simon-game/",
     },
     {
@@ -80,7 +80,7 @@ export default function Project() {
       description:
         "Using React, I have developed a TextUtils application that generates a comprehensive description of its features, including text transformation, analysis, cleaning, formatting, and clipboard operations, making it an essential tool for efficient text management.",
       githubLink: "https://github.com/asadkhaksar1122/Textutils",
-      imageurl: "./images/texturils.png",
+      imageurl: "./navbaricon/texturils.png",
       livelink: "https://asadkhaksar1122.github.io/Textutils/",
     },
     {
@@ -88,7 +88,7 @@ export default function Project() {
       description:
         "I have developed a note-taking application using HTML, CSS, and JavaScript. This application leverages the browser's local storage to store and manage notes.",
       githubLink: "https://github.com/asadkhaksar1122/Notes-app",
-      imageurl: "./images/notes.png",
+      imageurl: "./navbaricon/notes.png",
       livelink: "https://asadkhaksar1122.github.io/Notes-app/",
     },
     {
@@ -96,7 +96,7 @@ export default function Project() {
       description:
         "I have developed a web application using React components that serves as an emoji encyclopedia",
       githubLink: "https://github.com/asadkhaksar1122/emoji-wikipedia",
-      imageurl: "./images/emoji.jpeg",
+      imageurl: "./navbaricon/emoji.jpeg",
       livelink: "https://asadkhaksar1122.github.io/emoji-wikipedia/",
     },
     {
@@ -104,7 +104,7 @@ export default function Project() {
       description:
         "I have a lot of projects in my GitHub Account please visit and look there",
       githubLink: "https://github.com/asadkhaksar1122",
-      imageurl: "./images/otherproject.png",
+      imageurl: "./navbaricon/otherproject.png",
     },
   ];
   return (
